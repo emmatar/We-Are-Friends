dab7fdf108dadffc4348f860695068203b21d3b5
Elijah Matar <m.matar515@gmail.com>
2024-04-24 21:04:28 -0500

    User router file created to include request methods that link to corresponding controller

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    editUser,
+    deleteUser,
+    createFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// /api/users/
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(editUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
+
+module.exports = router;
